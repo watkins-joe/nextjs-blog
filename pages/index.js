a29fb185119bf2f8623d41a0e441221fd280eed0
watkins-joe <watkinsjoe99@live.com>
2022-01-19 18:27:35 -0600

    added blog section, getStaticProps usage, 'lib' directory, used gray matter library to parse the blog posts. now displays under profile

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,18 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +25,20 @@ export default function Home() {
           Iowa and a Fullstack Software Developer for the apps I'm making on the
           side.
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
